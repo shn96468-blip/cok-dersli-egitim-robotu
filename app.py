5320fb0ab4e81a683a90ec36f9d4f12fb63902b6
shn96468-blip <shn96468@gmail.com>
2025-11-10 16:17:38 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 # app.py, 105. satır civarı:
 def toggle_user_login_panel():
-    # 106. satır:
-    if st.session_state['user_login_allowed']: 
-        # 107. satır (GEREKLİ GIRINTILI BLOK):
+    if st.session_state['user_login_allowed']:
         st.session_state['show_user_login'] = not st.session_state['show_user_login']
         st.session_state['show_admin_login'] = False
         st.session_state['show_user_register'] = False
-    else: # 111. satır
+    else:
         st.sidebar.error("Üye girişi şu anda bakımdadır.")
+
+def toggle_user_register_panel():
+    if st.session_state['registration_allowed']:
+# ... (kodun geri kalanı 115. satırda devam ediyor)
