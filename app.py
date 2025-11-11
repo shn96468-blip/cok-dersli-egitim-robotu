5275baafa8f659796bdb54101defca2f764991f7
shn96468-blip <shn96468@gmail.com>
2025-11-11 08:29:30 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,4 @@
-# Öğrenci modunda başlık rengini beyaz yapalım
-    st.markdown(f'<style>h1, h2, h3, h4, h5, h6 {{color: #FFFFFF;}}</style>', unsafe_allow_html=True)
+import streamlit as st
+import time
+
+# --- YÖNETİCİ GİRİŞİ AYARLARI VE OTURUM BAŞLATMA ---
