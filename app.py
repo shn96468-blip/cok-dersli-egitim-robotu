89cc2933d7a3064d5bc75882707209a02bde9bf4
shn96468-blip <shn96468@gmail.com>
2025-11-11 08:21:26 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,36 +1,4 @@
-# OTURUM DURUMU BAŞLANGIÇ DEĞERLERİ (Bu kısımlar tam girintili olmalıdır)
-if 'admin_mode' not in st.session_state:
-    st.session_state['admin_mode'] = False
-if 'user_logged_in' not in st.session_state:
-    st.session_state['user_logged_in'] = False
-if 'current_user' not in st.session_state:
-    st.session_state['current_user'] = None
-if 'show_admin_login' not in st.session_state:
-    st.session_state['show_admin_login'] = False
-if 'show_user_login' not in st.session_state:
-    st.session_state['show_user_login'] = False
-if 'show_user_register' not in st.session_state:
-    st.session_state['show_user_register'] = False
-if 'app_color' not in st.session_state:
-    st.session_state['app_color'] = '#1E90FF'
-if 'announcement' not in st.session_state:
-    st.session_state['announcement'] = "🤖 Eğitim robotu aktif! Yeni konuları keşfetmeye başlayın."
-if 'announcement_color' not in st.session_state:
-    st.session_state['announcement_color'] = 'warning'
-if 'registration_allowed' not in st.session_state:
-    st.session_state['registration_allowed'] = True
-if 'user_login_allowed' not in st.session_state:
-    st.session_state['user_login_allowed'] = True
-if 'chat_history' not in st.session_state:
-    st.session_state['chat_history'] = []
-if 'secilen_ders' not in st.session_state:
-    st.session_state['secilen_ders'] = None
-if 'show_kanka_chat' not in st.session_state:
-    st.session_state['show_kanka_chat'] = False
-if 'music_enabled' not in st.session_state:
-    st.session_state['music_enabled'] = False
-if 'music_url' not in st.session_state:
-    st.session_state['music_url'] = "https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3"
+import streamlit as st
+import time
 
-# --- MODÜL VE KÜTÜPHANE İÇE AKTARMA (Hata Kontrollü) --- (Buradan itibaren kod girintisiz devam eder)
-# ...
+# --- YÖNETİCİ GİRİŞİ AYARLARI VE OTURUM BAŞLATMA ---
