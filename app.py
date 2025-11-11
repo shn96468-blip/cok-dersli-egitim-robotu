706dfada2d2892b34339e8ded6a265dfbc8f0d17
shn96468-blip <shn96468@gmail.com>
2025-11-11 08:50:36 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,2 @@
- for ders in DERSLER:
-        # 4 boşluk girinti
-        with ders["kolon"]: 
-            # 8 boşluk girinti
-            if st.button(f"{ders['simgesi']} {ders['isim']}", key=f"btn_{ders['isim']}", use_container_width=True):
-                # 12 boşluk girinti
-                st.session_state['secilen_ders'] = ders['isim']
-                st.rerun()
+ import streamlit as st
+import time
