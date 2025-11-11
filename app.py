8c371ec5179ef18b8de98f83183ee4c5b2e9c968
shn96468-blip <shn96468@gmail.com>
2025-11-11 08:36:44 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 # --- SOHBET VE ÇEVİRİ MANTIKLARI ---
-basit_sozluk = {
+basit_sozluk = 
     "merhaba": "Hello", "selam": "Hi", "teşekkürler": "Thanks", "sağol": "Thanks",
     "elma": "Apple", "armut": "Pear", "kedi": "Cat", "köpek": "Dog",
     "apple": "Elma", "pear": "Armut", "cat": "Kedi", "dog": "Köpek",
     "again": "Tekrar / Yine",
     "teach": "Öğretmek", "study": "Çalışmak", "kitap": "Book", "kalem": "Pen"
-} # BURAYA KAPATICI '}' GELDİ
+} # BURAYA KAPATICI '' GELDİ
+
